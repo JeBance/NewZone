9bb5dcb7cbdba5569e5c866280e7d869c5d4442e
Олег <JeBance>
2024-12-21 15:24:21 +0300

    added function dbInit

---
+++ assets/js/db.js
@@ -0,0 +1,35 @@
+async function dbInit()
+{
+	return new Promise((resolve, reject) => {
+		let openRequest = indexedDB.open("DB", 1);
+
+		openRequest.onupgradeneeded = function() {
+			let db = openRequest.result;
+			switch(event.oldVersion) {
+				case 0:
+					let contacts = db.createObjectStore('contacts', {keyPath: 'fingerprint'});
+					let messages = db.createObjectStore('messages', {keyPath: 'id'});
+					let nicknameIndex = contacts.createIndex('nickname_id', 'nickname');
+					let chatIndex = messages.createIndex('chat_id', 'chat');
+					break;
+			}
+		};
+
+		openRequest.onerror = function() {
+			reject('Error: ' + openRequest.error);
+		};
+
+		openRequest.onsuccess = function() {
+			let db = openRequest.result;
+			db.onversionchange = function() {
+				db.close();
+				reject('The database is out of date. Please reload the page to update.');
+			};
+			resolve(db);
+		};
+
+		openRequest.onblocked = function() {
+			reject('The database is out of date. Please close other tabs and reload the page to update.');
+		};
+	});
+}
